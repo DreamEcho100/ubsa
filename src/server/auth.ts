055a87c0cfb607e59a98e410f6450efd00ad3768
DreamEcho100 <maze6572198@gmail.com>
2023-02-20 18:57:39 +0200

    Minor update

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,13 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
 import { type GetServerSidePropsContext } from "next";
+
 import {
   getServerSession,
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "~/env.mjs";
+
 import { prisma } from "~/server/db";
 
 /**
